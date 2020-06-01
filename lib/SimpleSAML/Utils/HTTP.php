72398bafbed04cef6a75b44c075887855a98722b
Paul Dixon <paul@liblynx.com>
2020-06-01 11:57:34 +0100

    allowed detection of liblynx https

--- lib/SimpleSAML/Utils/HTTP.php
+++ lib/SimpleSAML/Utils/HTTP.php
@@ -83,6 +83,11 @@ class HTTP
             return false;
         }
 
+        //patch for LibLynx
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && ($_SERVER['HTTP_X_FORWARDED_PROTO']==='https')) {
+            return true;
+        }
+
         // otherwise, HTTPS will be a non-empty string
         return $_SERVER['HTTPS'] !== '';
     }
